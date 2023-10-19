529874522fb86c6bf7d0d8e5b3edfa8712789f6d
ssamarin <semenssamarin@mail.ru>
2023-10-19 19:24:11 +1000

    refactor(Components): replace action creators to createAction

--- src/store/index.js
+++ src/store/index.js
@@ -11,16 +11,10 @@ const stringMiddleware = () => next => action => {
     return next(action);
 };
 
-// const store = createStore( 
-//                     combineReducers({heroes, filters}), 
-//                     compose(applyMiddleware(ReduxThunk, stringMiddleware), 
-//                             window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-//                     );
-
 const store = configureStore({
     reducer: {heroes, filters},
     middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
     devTools: process.env.NODE_ENV !== 'production',
 });
 
-export default store;
+export default store
