7abe17992c769df3f1bed82a68af8af9acdbe6b6
ssamarin <semenssamarin@mail.ru>
2023-10-21 19:15:03 +1000

    refactor(reducers): replace creating reduce to createReducer

--- src/reducers/heroes.js
+++ src/reducers/heroes.js
@@ -1,38 +1,37 @@
+import { createReducer } from "@reduxjs/toolkit";
+
+import {
+    heroesFetching,
+    heroesFetched,
+    heroesFetchingError,
+    heroCreated,
+    heroDeleted,
+} from '../actions';
+
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle',
 }
 
-const heroes = (state = initialState, action) => {
-    switch (action.type) {
-        case 'HEROES_FETCHING':
-            return {
-                ...state,
-                heroesLoadingStatus: 'loading'
-            }
-        case 'HEROES_FETCHED':
-            return {
-                ...state,
-                heroes: action.payload,
-                heroesLoadingStatus: 'idle'
-            }
-        case 'HEROES_FETCHING_ERROR':
-            return {
-                ...state,
-                heroesLoadingStatus: 'error'
-            }
-        case 'HERO_CREATED':
-            return {
-                ...state,
-                heroes: [...state.heroes, action.payload],
-            }
-        case 'HERO_DELETED': 
-            return {
-                ...state,
-                heroes: state.heroes.filter(item => item.id !== action.payload),
-            }
-        default: return state
-    }
-}
+const heroes = createReducer(initialState, builder => { // builder - обьект который позволяет конструировать редусер при помощи 3 встроеных в него методов
+    builder
+        .addCase(heroesFetching, state => { // аналог Case из reducer, принимает в себя 2 аргумента - актион креатор и функцию по изменению стейта 
+            state.heroesLoadingStatus = 'loading';
+        }) // addCease ничего не возвращает и только в таком случае будет работать иммер js 
+        .addCase(heroesFetched, (state, action) => {
+            state.heroesLoadingStatus = 'idle';
+            state.heroes = action.payload;
+        })
+        .addCase(heroesFetchingError, state => {
+            state.heroesLoadingStatus = 'error';
+        })
+        .addCase(heroCreated, (state, action) => {
+            state.heroes.push(action.payload);
+        })
+        .addCase(heroDeleted, (state, action) => {
+            state.heroes = state.heroes.filter(item => item.id !== action.payload)
+        })
+        .addDefaultCase(() => {});
+});
 
 export default heroes;
